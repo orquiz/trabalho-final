b875a54b6816faec8f48c48d38198d4c8c79d243
orquiz <142925160+orquiz@users.noreply.github.com>
2023-09-18 13:57:36 -0300

    Update and rename README.md to index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+# trabalho-final
+ nosso programa contém 4 funções :
+
+A primeira faz com que o usuário crie a sua própria tarefa.
+
+A segunda faz com que o usuário consiga ver sua lista de tarefas criada por ele.
+
+A terceira tem a funcionalidade do usuário poder editar sua tarefa.
+
+E a quarta com que o usuário apague sua tarefa.
+
+Abaixo das funções temos um loop com que faça que o usuário crie mais de uma tarefa e decida o que ele quer fazer a seguir.
